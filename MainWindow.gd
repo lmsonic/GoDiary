446fcc9af3bf3beaf2d29cd294f7911ccecbb626
Iride Lombardi <lorenzomassidda@hotmail.com>
2022-06-22 21:12:01 +0200

    Dragging

--- MainWindow.gd
+++ MainWindow.gd
@@ -1,31 +1,52 @@
 extends Control
 
-onready var home:=$Windows/Home
-onready var calendar:=$Windows/Calendar
-onready var graph:=$Windows/Graph
-onready var tasks:=$Windows/Tasks
+onready var home:Control=$Windows/Home
+onready var calendar:Control=$Windows/Calendar
+onready var graph:Control=$Windows/Graph
+onready var tasks:Control=$Windows/Tasks
 
 onready var tween:Tween=$Tween
 
-onready var windows_container:=$Windows
+onready var windows_container:Container=$Windows
 
+var current_window_index:int
 var windows:=[]
 
 func _ready() -> void:
 	windows = windows_container.get_children()
 	move_window(home)
-	windows_container.rect_size.x = rect_size.x * 4.0
+	windows_container.rect_size.x = rect_size.x * windows.size()
 
-
+func tween_window( target_x:float, duration:float):
+	tween.interpolate_property(windows_container, "rect_position:x",
+		windows_container.rect_position.x,target_x, duration)
+	tween.start() 
 
 func move_window(chosen_window):
-	var order := windows.find(chosen_window)
-	tween.interpolate_property(windows_container, "rect_position:x",
-		windows_container.rect_position.x,-order * rect_size.x, 0.1)
-	tween.start()
-
+	var index := windows.find(chosen_window)
+	current_window_index = index
+	tween_window(-index*rect_size.x,0.2)
 
 	
+var start_drag_x:float
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenDrag:
+		windows_container.rect_position.x += event.relative.x
+	elif event is InputEventScreenTouch:
+		if event.pressed:
+			start_drag_x=event.position.x
+		else:
+			var drag_dist = start_drag_x - event.position.x
+			if drag_dist > rect_size.x * 0.4 \
+				and current_window_index+1 < windows.size():
+				
+				move_window(windows[current_window_index+1])
+			elif drag_dist < -rect_size.x * 0.4 \
+				and current_window_index - 1 >= 0:
+				move_window(windows[current_window_index-1])
+			else:
+				move_window(windows[current_window_index])
 
 func _on_HomeButton_pressed() -> void:
 	move_window(home)
@@ -36,10 +57,8 @@ func _on_CalendarButton_pressed() -> void:
 func _on_GraphButton_pressed() -> void:
 	move_window(graph)
 
-
 func _on_TasksButton_pressed() -> void:
 	move_window(tasks)
 
-
-func _on_MainWindow_resized() -> void:
-	windows_container.rect_size.x = rect_size.x * 4.0
+func reset_window_container() -> void:
+	windows_container.rect_size.x = rect_size.x * windows.size()
