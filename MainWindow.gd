95238c361d9376debc8d6a18d5a32948096d317b
Iride Lombardi <lorenzomassidda@hotmail.com>
2022-06-22 20:32:36 +0200

    Tweening

--- MainWindow.gd
+++ MainWindow.gd
@@ -1,33 +1,45 @@
 extends Control
 
-onready var home:=$Home
-onready var calendar:=$Calendar
-onready var graph:=$Graph
-onready var tasks:=$Tasks
+onready var home:=$Windows/Home
+onready var calendar:=$Windows/Calendar
+onready var graph:=$Windows/Graph
+onready var tasks:=$Windows/Tasks
+
+onready var tween:Tween=$Tween
+
+onready var windows_container:=$Windows
 
 var windows:=[]
 
 func _ready() -> void:
-	windows.append_array([home,calendar,graph,tasks])
-	show_window(home)
+	windows = windows_container.get_children()
+	move_window(home)
+	windows_container.rect_size.x = rect_size.x * 4.0
 
 
-func show_window(chosen_window:Control):
-	for window in windows:
-		window.visible = window==chosen_window
 
+func move_window(chosen_window):
+	var order := windows.find(chosen_window)
+	tween.interpolate_property(windows_container, "rect_position:x",
+		windows_container.rect_position.x,-order * rect_size.x, 0.1)
+	tween.start()
+
+
+	
 
 func _on_HomeButton_pressed() -> void:
-	show_window(home)
-
+	move_window(home)
 
 func _on_CalendarButton_pressed() -> void:
-	show_window(calendar)
-
+	move_window(calendar)
 
 func _on_GraphButton_pressed() -> void:
-	show_window(graph)
+	move_window(graph)
 
 
 func _on_TasksButton_pressed() -> void:
-	show_window(tasks)
+	move_window(tasks)
+
+
+func _on_MainWindow_resized() -> void:
+	windows_container.rect_size.x = rect_size.x * 4.0
