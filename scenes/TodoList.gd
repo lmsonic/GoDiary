c5b478b65f2ee430cd8b4f4976ed6e74a5b9b181
Maelonlon <44503731+Maelonlon@users.noreply.github.com>
2022-06-24 12:28:49 +0200

    moving up Input

--- scenes/TodoList.gd
+++ scenes/TodoList.gd
@@ -4,11 +4,16 @@ extends PanelContainer
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+onready var old_pos :float = $Input.rect_position.y
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+	
+func _process(delta):
+	if is_virtual_keyboard_shown():
+		var kb_pos = OS.get_virtual_keyboard_height()
+		$Input.rect_position.y -= kb_pos.y
+	else:
+		$Input.rect_position.y = old_pos
 
 func add_todo(text):
 	var todo_item = Label.new()
@@ -18,4 +23,5 @@ func add_todo(text):
 #func _process(delta):
 #	pass
 
-
+func is_virtual_keyboard_shown():
+	return OS.get_virtual_keyboard_height() != 0
