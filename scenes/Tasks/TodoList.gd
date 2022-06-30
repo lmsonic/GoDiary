d8e2ef54cc754ee94796a91b0641375000f4d86b
Maelonlon <44503731+Maelonlon@users.noreply.github.com>
2022-06-30 04:44:41 +0200

    tasks tree wip

--- scenes/Tasks/TodoList.gd
+++ scenes/Tasks/TodoList.gd
@@ -1,6 +1,6 @@
 extends PanelContainer
 
-onready var todo_list:= $VBoxContainer/TodoList
+onready var todo_list:= $VBoxContainer/Tree
 onready var line_edit:LineEdit= $VBoxContainer/Input/LineEdit
 onready var input:= $VBoxContainer/Input
  
@@ -19,9 +19,15 @@ func _process(delta):
 		input.rect_position.y = old_height
 
 func add_todo(text):
-	var todo_item = Label.new()
-	todo_item.text = text
+	todo_list.create_item()
+	var todo_item = HBoxContainer.new()
 	todo_list.add_child(todo_item)
+	var todo_check = CheckBox.new()
+	todo_item.add_child(todo_check)
+	var todo_text = Label.new()
+	todo_text.text = text
+	todo_item.add_child(todo_text)
+	
 	OS.hide_virtual_keyboard()
 
 
